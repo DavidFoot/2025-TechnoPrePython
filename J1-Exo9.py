24b247d467e022175f700f665eebb008090f031f
David Foot <david.foot@outlook.be>
2025-02-24 16:21:23 +0100

    Jour 1 - Exo9

---
+++ J1-Exo9.py
@@ -0,0 +1,34 @@
+"""
+Mélangeur de mots
+Demande trois mots à l’utilisateur.
+Affiche-les dans un ordre aléatoire ou avec un effet rigolo :
+Exemple 1 : Répète chaque mot deux fois : "chat chat chien chien souris souris".
+Exemple 2 : Affiche-les en majuscules inversées : "TAHC NEIHC SIURIS" """
+from ntpath import split
+
+from random import shuffle
+
+def MuliplyEachItem(list,x):
+    listCopy = []
+    for item in list:
+        for i in range(0,x):
+            listCopy.append(item)
+    return listCopy
+
+def UpperRevertStringJoined(myList):
+    listCopy = []
+    for item in myList:
+        listCopy.append(''.join(reversed((item).upper())))
+    return listCopy
+
+wordList = []
+for i in range(0,3):
+    wordList.append(input(f"Entrez le mot {i+1}: "))
+shuffle(wordList)
+print(wordList)
+print(MuliplyEachItem(wordList,5))
+[print(x,end="-") for x in wordList]
+print()
+[print(x,end="-") for x in UpperRevertStringJoined(wordList)]
+
+
